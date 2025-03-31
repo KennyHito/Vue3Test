0dab9b28a663c77d0964c9a839700183deb2133b
yuhaitao <yuhaitao1@lczq.com>
2025-03-31 10:34:44 +0800

    modify: 更新

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
 	lintOnSave:false, //关闭语法检查
+	pages: {
+    index: {
+      //在这里每次更改入口路劲地址即可切换
+      // entry: './src/main.js',
+      entry: './01_src_分析工程结构/main.js',
+    },
+  },
 }
