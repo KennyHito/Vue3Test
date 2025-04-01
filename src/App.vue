7be58dea3bd52ef8a0cb601d7da78f077ddd8b15
yuhaitao <yuhaitao1@lczq.com>
2025-04-01 09:24:02 +0800

    modify: update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
 	<div class="app">
-		<h3>我是App组件</h3>
-		<Suspense>
-			<template v-slot:default>
-				<Child/>
-			</template>
-			<template v-slot:fallback>
-				<h3>稍等，加载中...</h3>
-			</template>
-		</Suspense>
+		初始化内容
 	</div>
 </template>
 
 <script>
-	// import Child from './components/Child'//静态引入
-	import {defineAsyncComponent} from 'vue' 
-	const Child = defineAsyncComponent(()=>import('./components/Child')) //异步引入
 	export default {
 		name:'App',
-		components:{Child},
 	}
 </script>
 
 <style>
 	.app{
-		background-color: gray;
+		background-color: orange;
+		border-radius: 10px;
 		padding: 10px;
 	}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
 	pages: {
     index: {
       //在这里每次更改入口路劲地址即可切换
-      // entry: './src/main.js',
-      entry: './03_src_ref函数/main.js',
+      entry: './src/main.js',
+      // entry: './21_src_Suspense/main.js',
     },
   },
 }
