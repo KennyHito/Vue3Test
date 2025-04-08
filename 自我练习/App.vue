e231ff340ca411318050d83d13b551c628424623
yuhaitao <yuhaitao1@lczq.com>
2025-04-08 16:30:47 +0800

    modify: 自定义ref

--- 自我练习/App.vue
+++ 自我练习/App.vue
@@ -1,14 +1,17 @@
 <template>
 	<Test />
+	<br />
+	<CustomRef />
 </template>
 
 <script>
 import Test from './components/Test.vue'
-
+import CustomRef from './components/CustomRef.vue';
 export default {
 	name: 'App',
 	components: {
-		Test
+		Test,
+		CustomRef
 	}
 }
 </script>
