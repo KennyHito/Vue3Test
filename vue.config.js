40cf2f47cf253d76a1caa0dcb6f71ba0818f3deb
yuhaitao <yuhaitao1@lczq.com>
2025-04-09 16:40:08 +0800

    modify: 更新

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	lintOnSave:false, //关闭语法检查
 	pages: {
     index: {
-      //在这里每次更改入口路劲地址即可切换
+      // ⚠️ 在这里每次更改入口路劲地址即可切换 ⚠️
       // entry: './src/main.js',
       // entry: './12_src_自定义hook/main.js',
       entry: './22_src_自我练习/main.js',
