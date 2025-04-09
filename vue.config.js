dccb89980e5d17cfdd65c84ed2a2a5571185cb4e
yuhaitao <yuhaitao1@lczq.com>
2025-04-09 11:02:58 +0800

    modify: 自我练习

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       //在这里每次更改入口路劲地址即可切换
       // entry: './src/main.js',
       // entry: './12_src_自定义hook/main.js',
-      entry: './自我练习/main.js',
+      entry: './22_src_自我练习/main.js',
     },
   },
 }
